c51779dc7ec240c500fc3c6d9b27f85e8d8bbf39
Xoric <xoric@gmx.net>
2013-12-28 13:26:54 +0100

    improved Map

--- PrismData/src/xoric/prism/common/Common.java
+++ PrismData/src/xoric/prism/common/Common.java
@@ -15,4 +15,14 @@ public abstract class Common
 
 		return s;
 	}
+
+	public static int max(int i, int j)
+	{
+		return i > j ? i : j;
+	}
+
+	public static int min(int i, int j)
+	{
+		return i < j ? i : j;
+	}
 }
