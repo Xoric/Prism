299a15ed8facea1edd3bda5cc11f2f0d7fb66f1d
Xoric <xoric@gmx.net>
2014-01-30 12:48:14 +0100

    updated UI and internal file structure of Collections

--- PrismCreatorSpriteCollection/src/xoric/prism/creator/custom/model/SpriteNames.java
+++
@@ -1,9 +0,0 @@
-package xoric.prism.creator.custom.model;
-
-public class SpriteNames
-{
-	public static String getFilename(int spriteIndex, int variation)
-	{
-		return "sprite" + spriteIndex + ".var" + variation + ".png";
-	}
-}
--- PrismCreatorSpriteCollection/src/xoric/prism/creator/custom/view/IObjectListListener.java
+++
@@ -1,6 +0,0 @@
-package xoric.prism.creator.custom.view;
-
-public interface IObjectListListener
-{
-	public void displayObject();
-}
