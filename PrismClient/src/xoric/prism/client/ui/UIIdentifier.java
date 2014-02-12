345701146a4b727fa57185e8398b64b09616b8b3
Xoric <xoric@gmx.net>
2014-02-12 16:28:40 +0100

    added safe valueOf method

---
+++ PrismClient/src/xoric/prism/client/ui/UIIdentifier.java
@@ -0,0 +1,28 @@
+package xoric.prism.client.ui;
+
+import xoric.prism.data.exceptions.PrismException;
+import xoric.prism.data.exceptions.UserErrorText;
+
+public enum UIIdentifier
+{
+	NONE, WINDOW, FRAME, BUTTON, LABEL;
+
+	private static final UIIdentifier[] VALUES = values();
+
+	public static UIIdentifier valueOf(int v) throws PrismException
+	{
+		if (v < 0 || v >= VALUES.length)
+		{
+			PrismException e = new PrismException();
+			// ----
+			e.user.setText(UserErrorText.INTERNAL_PROBLEM);
+			// ----
+			e.code.setText("error while converting int to " + UIIdentifier.class.toString());
+			e.code.addInfo("value", v);
+			e.code.addInfo("enum count", VALUES.length);
+			// ----
+			throw e;
+		}
+		return VALUES[v];
+	}
+}
