9bee6caa40f0ad5d464002fc855140333c61553f
Xoric <xoric@gmx.net>
2014-02-13 14:44:48 +0100

    implemented DefaultShaderSubstitute class

---
+++ PrismSceneLWJGL/src/xoric/prism/scene/lwjgl/shaders/DefaultShaderSubstitute.java
@@ -0,0 +1,27 @@
+package xoric.prism.scene.lwjgl.shaders;
+
+import org.lwjgl.opengl.GL11;
+
+import xoric.prism.data.types.PrismColor;
+import xoric.prism.scene.shaders.IShader2;
+import xoric.prism.scene.textures.ITexture;
+
+public class DefaultShaderSubstitute implements IShader2
+{
+	@Override
+	public void activate()
+	{
+	}
+
+	@Override
+	public void setTexture(ITexture texture)
+	{
+		GL11.glBindTexture(GL11.GL_TEXTURE_2D, 0); // TODO: needed?
+		GL11.glBindTexture(GL11.GL_TEXTURE_2D, texture.getTextureID());
+	}
+
+	@Override
+	public void setColor(PrismColor color)
+	{
+	}
+}
