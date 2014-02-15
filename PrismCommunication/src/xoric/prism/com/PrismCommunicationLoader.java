e54a881bccae6bb6f48c90d68cbf857718e69c86
Xoric <xoric@gmx.net>
2014-02-15 17:47:10 +0100

    integrated changes to IStackable interface

--- PrismCommunication/src/xoric/prism/com/PrismCommunicationLoader.java
+++ PrismCommunication/src/xoric/prism/com/PrismCommunicationLoader.java
@@ -4,12 +4,12 @@ import xoric.prism.data.exceptions.PrismException;
 import xoric.prism.data.global.CommonIndex;
 import xoric.prism.data.global.FileTableDirectoryIndex;
 import xoric.prism.data.global.Prism;
-import xoric.prism.data.heap.Heap;
-import xoric.prism.data.meta.MetaBlock;
+import xoric.prism.data.heap.Heap_in;
+import xoric.prism.data.meta.MetaBlock_in;
 import xoric.prism.data.meta.MetaFile;
 import xoric.prism.data.meta.MetaKey;
-import xoric.prism.data.meta.MetaLine;
-import xoric.prism.data.meta.MetaList;
+import xoric.prism.data.meta.MetaLine_in;
+import xoric.prism.data.meta.MetaList_in;
 import xoric.prism.data.meta.MetaType;
 
 public abstract class PrismCommunicationLoader
@@ -22,11 +22,11 @@ public abstract class PrismCommunicationLoader
 	private static void loadMessageUrgency() throws PrismException
 	{
 		MetaFile f = Prism.global.loadMetaFile(FileTableDirectoryIndex.COMMON, CommonIndex.URGENCY.ordinal());
-		MetaList metaList = f.getMetaList();
-		MetaBlock metaBlock = metaList.claimMetaBlock(MetaType.URGENCY);
-		MetaLine ml = metaBlock.claimLine(MetaKey.ITEM);
+		MetaList_in metaList = f.getMetaList();
+		MetaBlock_in metaBlock = metaList.claimMetaBlock(MetaType.URGENCY);
+		MetaLine_in ml = metaBlock.claimLine(MetaKey.ITEM);
 		ml.ensureMinima(3, 0, 0);
-		Heap h = ml.getHeap();
+		Heap_in h = ml.getHeap();
 
 		for (int i = 0; i < 3; ++i)
 			MessageDispatcher.thresholds[i] = h.ints.get(i);
