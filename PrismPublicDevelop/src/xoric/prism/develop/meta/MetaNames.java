ca214a42c9a22117074ebb8c754610694d3174b8
Xoric <xoric@gmx.net>
2014-03-02 16:15:37 +0100

    standardized names for MetaBlock files

--- PrismPublicDevelop/src/xoric/prism/develop/meta/MetaNames.java
+++ PrismPublicDevelop/src/xoric/prism/develop/meta/MetaNames.java
@@ -1,10 +1,17 @@
 package xoric.prism.develop.meta;
 
+import xoric.prism.data.meta.MetaType;
 
 public abstract class MetaNames
 {
+	@Deprecated
 	public static String makeMetaBlock(String filename)
 	{
 		return filename.toLowerCase() + ".block";
+	}
+
+	public static String makeMetaBlock(MetaType t)
+	{
+		return t.toString().toLowerCase() + ".block";
 	}
 }
